3b7e93e9602b5acf823f255d03ae1932c44828e9
Pickering <pickefr@prd.manulifeusa.com>
2020-02-28 10:11:59 -0500

    Combine files

--- TomTom API.R
+++ TomTom API.R
@@ -20,10 +20,10 @@ refTable <-
     city = c("beijing", "shanghai", "hong-kong", "milan", "wuhan", "shenzhen", "singapore")
   )
 
-refTable <- 
-  readRDS("cityData.Rds") %>% 
+refTable <-
+  readRDS("cityData.Rds") %>%
   select(
-    country = country, 
+    country = country,
     city = href_cities
   )
 
@@ -34,7 +34,7 @@ baseApi <- glue("https://api.midway.tomtom.com/ranking/live/{refTable[['country'
 # Gather Api Data #
 #-----------------#
 
-rawTrafficData <- 
+rawTrafficData <-
   pmap(
     list(
       url = baseApi,
@@ -42,65 +42,79 @@ rawTrafficData <-
     ),
     safely({
       function(url, city){
-        
+
         print(glue("Collecting data for {city}"))
-        
+
         # Raw data from API
         response <- GET(url)
-        
+
         # Assertions
         assert_that(
           status_code(response) == 200,
           msg = "Issue with GET request"
         )
-        
+
         # Response header
         tradfficHeader <- content(response)[[1]]
         # Response body
         trafficList <- content(response)[[2]]
-        
+
         # Manipulate Data
-        trafficList %>% 
+        trafficList %>%
           map(
             safely({
               function(x){
                 unlist(x)
-              } 
+              }
             })
-          ) %>% 
-          map(~{.x$result}) %>% 
-          reduce(function(x, y){bind_rows(x, y)}) %>% 
+          ) %>%
+          map(~{.x$result}) %>%
+          reduce(function(x, y){bind_rows(x, y)}) %>%
           mutate(
-            UpdateTime = as.POSIXct(UpdateTime / 1000, origin = "1970-01-01", timezone = "America/Boston"),
+            UpdateTime = as.POSIXct(UpdateTime / 1000, origin = "1970-01-01", timezone = "UTC"),
             city = city
-          ) %>% 
+          ) %>%
           select(
             city                   = city,
-            date_time              = UpdateTime, 
-            traffic_index_live     = TrafficIndexLive, 
+            date_time              = UpdateTime,
+            traffic_index_live     = TrafficIndexLive,
             traffic_index_historic = TrafficIndexHistoric,
             jams_count             = JamsCount,
-            jams_length            = JamsLength, 
+            jams_length            = JamsLength,
             jams_delay             = JamsDelay
           )
-      }      
+      }
     })
-  ) %>% 
+  ) %>%
   set_names(refTable[["city"]])
 
 
-trafficErrors <- 
-  rawTrafficData %>% 
-  map(~{.x$error}) %>% 
+trafficErrors <-
+  rawTrafficData %>%
+  map(~{.x$error}) %>%
   compact()
-  
 
-trafficData <- 
-  map(function(x){x$result}) %>% 
+
+trafficData <-
+  map(function(x){x$result}) %>%
   reduce(function(x, y){bind_rows(x, y)})
 
-#--------------#  
+#--------------#
 # Upload to DB #
 #--------------#
 
+readr::write_csv(trafficData, "trafficData.csv")
 
+channel <- dbConnect(odbc(), dsn = "EricMart_PostgreSQL")
+
+dbDataDate <-
+  channel %>%
+  tbl("tom_tom") %>%
+  collect() %>%
+  summarize(city_date = max(date_time))
+
+uploadData <- trafficData %>% filter(date_time > dbDataDate[['city_date']])
+
+dbWriteTable(channel, "tom_tom", uploadData, append = T)
+
+dbDisconnect(channel)
